15833f50d2d8a595618d014790d6233aa4cc2f64
SalimMessaad1 <109216692+SalimMessaad1@users.noreply.github.com>
2024-10-22 12:32:37 +0100

    Update histogram_enhance.py

--- histogram_enhance.py
+++ histogram_enhance.py
@@ -5,7 +5,6 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Function to apply histogram equalization
 def enhance_image_with_histogram(image_path):
     # Read the image in grayscale mode
     img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
@@ -35,6 +34,6 @@ def enhance_image_with_histogram(image_path):
     cv2.imwrite(output_path, equalized_img)
     print(f"Enhanced image saved as {output_path}")
 
-# Example usage
-image_path = 'C:/Users/INFO/Downloads/pyazyuhcmrb71.jpg'  # Provide the path to your image file
+# call functions 
+image_path = '/path/to/image.jpg'  # Provide the path to your image file
 enhance_image_with_histogram(image_path)
